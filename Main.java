252e964f19e6be5acc93958868bccfab3151ebc4
PaboluManohar <pabolumanohar1@gmail.com>
2024-07-09 16:33:20 +0530

    added one more line

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public  class Main{
         System.out.println("hi my name is manohar");
         System.out.println("this is second java pro to test jenkins");
         System.out.println(new Date().toString());
+        System.out.println("this is new text after creating jenkins");
 
     }
 }
