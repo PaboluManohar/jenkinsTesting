06de94b85f9ab18030f11211aac8013fde3fd371
PaboluManohar <pabolumanohar1@gmail.com>
2024-07-09 14:26:38 +0530

    create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public  class Main{
+    public static void main(String[] args) {
+        System.out.println("hi my name is manohar");
+        System.out.println("this is second java pro to test jenkinss.");
+        System.out.println(new Date().toString());
+
+    }
+}
